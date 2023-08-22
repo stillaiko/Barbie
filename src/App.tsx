a3ad08fadbef9a61cb30522bc9c4e9be7a47958f
stillaiko <ana.ribeiro5@estudante.ifms.edu.br>
2023-08-22 08:05:54 -0400

    iniciando footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
+// import Footer from './componentes/footer/Footer'
+import Footer1 from './componentes/footer1/Footer'
 
 
 function App() {
   return (
-   <><Header /><Footer /></>
+   <>
+   <Header />
+   {/* <Footer /> */}
+   <Footer1/>
+   </>
   )
 }
 
---
+++ src/componentes/footer1/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+import logo from '../../assets/Barbie_Branco.webp'
+
+function Footer(){
+return(
+<footer>
+
+<img src={logo} alt="" />
+    
+</footer>
+)
+}
+
+export default Footer
